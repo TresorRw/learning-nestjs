888d5050cce4c784f5cb3190bc84b26e84e4503b
TresorRw <alaintresorcyusa683@gmail.com>
2023-06-13 23:08:45 +0200

    Finished prisma module and started auth DTO

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
-  signin() {
-    return { message: 'On sign in' };
-  }
+  constructor(private prisma: PrismaService) {}
+
   signup() {
     return { message: 'On sign up' };
+  }
+
+  signin() {
+    return { message: 'On sign in' };
   }
 }
