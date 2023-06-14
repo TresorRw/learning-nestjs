4ec35520d1d2bc10ba93bf23b8a4f302e1a6ca77
TresorRw <alaintresorcyusa683@gmail.com>
2023-06-14 21:15:04 +0200

    Started sign in functionalities

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,14 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
@@ -21,7 +29,29 @@ export class AuthService {
     }
   }
 
-  async signin() {
-    return { message: 'On sign in' };
+  async signin(dto: AuthDTO) {
+    // Find user by email address
+    const matchUser = await this.prisma.user.findFirst({
+      where: { email: dto.email },
+    });
+    if (matchUser) {
+      // Match password
+      const matchPwd = await argon.verify(matchUser.password, dto.password);
+      if (matchPwd) {
+        throw new HttpException(
+          {
+            statusCode: HttpStatus.ACCEPTED,
+            message: 'Login successfully',
+          },
+          HttpStatus.ACCEPTED,
+        );
+      } else {
+        throw new BadRequestException('Incorrect password');
+      }
+    } else {
+      throw new NotFoundException(
+        'These credentials are not familiar to us, try again',
+      );
+    }
   }
 }
